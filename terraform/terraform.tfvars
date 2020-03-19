33a45137f8e2c09db5f139957fddb66de41dc35e
Herb Stahl <ghstahl@gmail.com>
2020-03-19 10:22:29 -0700

    terraform init

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,4 @@
+az_resource_group_location = "eastus2"
+az_resource_group_name = "rg-heidi"
+
+# export ARM_ACCESS_KEY=$(az keyvault secret show --name terraform-backend-key --vault-name kv-tf-heidi --query value -o tsv)
---
+++ terraform/variables.tf
@@ -0,0 +1,20 @@
+variable "az_resource_group_name" {
+  description = "(Required) The name of the resource group where resources will be created."
+  type        = string
+}
+
+variable "az_resource_group_location" {
+  description = "(Required) The location where the resource group will reside."
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to help identify various services."
+  type        = map
+  default = {
+    DeployedBy     = "terraform"
+    Environment    = "prod"
+    OwnerEmail     = "DL-P7-OPS@p7.com"
+    Platform       = "na" # does not apply to us.
+  }
+}
