b45d2019d936d6144d77786a8f659c6e3c35ad68
Herb Stahl <ghstahl@gmail.com>
2020-03-24 15:48:18 -0700

    intiail

---
+++ src/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "azurerm" {
+    # Due to a limitation in backend objects, variables cannot be passed in.
+    # Do not declare an access_key here. Instead, export the
+    # ARM_ACCESS_KEY environment variable.
+
+    storage_account_name  = "stterraformheidi"
+    container_name        = "tstate"
+    key                   = "terraform.tfstate"
+  }
+}
+# Configure the Azure provider
+provider "azurerm" {
+  version = "=1.44.0"
+}
+resource "azurerm_resource_group" "rg" {
+  name     = var.az_resource_group_name
+  location = var.az_resource_group_location
+}
+
